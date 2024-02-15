f270e15ae9fa0f7588c198d7636ac4797c5c72cf
Pie9706 <pietro.montalbano9706@gmail.com>
2024-02-15 12:49:12 +0100

    feat(ecommerce):added tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,64 @@
-function App() {
+import { useState, useEffect } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  thumbnail: string;
+}
+
+function Ecommerce() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [cartItems, setCartItems] = useState<number[]>([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    try {
+      const response = await fetch(
+        "https://mockend.up.railway.app/api/products"
+      );
+      const result = await response.json();
+      setProducts(result);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+
+  function onClickCart(product: Product) {
+    setCartItems([...cartItems, product.id]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <p id="counter">{cartItems.length}</p>
+      <div id="title">
+        <h1>Ecommerce</h1>
+        <h2>Description</h2>
+      </div>
+      <div id="footer">
+        <div>
+          <ul>
+            {products.map((product: Product, index: number) => (
+              <li key={index}>
+                {product.title}
+                <img src={product.image} alt={product.thumbnail}></img>
+                {product.description}
+                {product.price}
+                <button onClick={() => onClickCart(product)}>
+                  Add to Cart
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Ecommerce;
