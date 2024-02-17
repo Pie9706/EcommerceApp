0e9d8711051536caf57f6185763e47743ce4c420
Pie9706 <pietro.montalbano9706@gmail.com>
2024-02-17 23:07:57 +0100

    change visualization products in home

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function Ecommerce() {
   const [products, setProducts] = useState<Product[]>([]);
   const [cartItems, setCartItems] = useState<Product[]>([]);
   const [showCart, setShowCart] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
   useEffect(() => {
     fetchProducts();
@@ -35,25 +36,21 @@ function Ecommerce() {
     const existingItem = cartItems.find((item) => item.id === product.id);
 
     if (existingItem) {
-      // Se l'elemento è già presente nel carrello, aumenta solo la quantità
       setCartItems((prevCartItems) =>
         prevCartItems.map((item) =>
           item.id === product.id ? { ...item, qty: item.qty + 1 } : item
         )
       );
-      // Decrementa la quantità disponibile nel catalogo
       setProducts((prevProducts) =>
         prevProducts.map((p) =>
           p.id === product.id ? { ...p, qty: p.qty - 1 } : p
         )
       );
     } else {
-      // Altrimenti, aggiungi un nuovo elemento al carrello con qty 1
       setCartItems((prevCartItems) => [
         ...prevCartItems,
         { ...product, qty: 1 },
       ]);
-      // Decrementa la quantità disponibile nel catalogo
       setProducts((prevProducts) =>
         prevProducts.map((p) =>
           p.id === product.id ? { ...p, qty: p.qty - 1 } : p
@@ -69,6 +66,15 @@ function Ecommerce() {
   function toggleCart() {
     setShowCart(!showCart);
   }
+
+  function toggleProductDetails(product: Product) {
+    if (selectedProduct && selectedProduct.id === product.id) {
+      setSelectedProduct(null);
+    } else {
+      setSelectedProduct(product);
+    }
+  }
+
   function onRemoveFromCart(product: Product) {
     const existingItem = cartItems.find((item) => item.id === product.id);
 
@@ -105,7 +111,12 @@ function Ecommerce() {
             onClick={toggleCart}
             className="bg-yellow-500 text-white px-2 py-2 rounded-full hover:bg-yellow-600 transition duration-300 mr-4"
           >
-            {showCart ? "Home" : "Carrello"}({cartItems.length})
+            {showCart
+              ? "Home"
+              : `Carrello (${cartItems.reduce(
+                  (total, item) => total + item.qty,
+                  0
+                )})`}
           </button>
         </div>
       </div>
@@ -116,7 +127,6 @@ function Ecommerce() {
           <button className="mt-2 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             BUY NOW
           </button>
-
           <div>
             {cartItems.map((item: Product) => (
               <div key={item.id} className="bg-white p-4 shadow-md rounded-md">
@@ -128,12 +138,11 @@ function Ecommerce() {
                 />
                 <p className="mt-2">Quantity: {item.qty}</p>
                 <p className="mt-2">${item.price}</p>
-
                 <button
                   onClick={() => onRemoveFromCart(item)}
                   className="mt-2 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                  Remove
+                  Remove from cart
                 </button>
               </div>
             ))}
@@ -146,22 +155,37 @@ function Ecommerce() {
         >
           {products.map((product: Product) => (
             <div key={product.id} className="bg-white p-4 shadow-md rounded-md">
-              <h3 className="text-lg font-semibold mb-2">{product.title}</h3>
+              <h3
+                onClick={() => toggleProductDetails(product)}
+                className="text-lg font-semibold mb-2 cursor-pointer"
+              >
+                {product.title}
+              </h3>
               <img
                 src={product.image}
                 alt={product.thumbnail}
                 className="mt-2 w-full h-32 object-cover"
               />
-              <p className="mt-2">{product.description}</p>
-              <p className="mt-2">Quantity: {product.qty}</p>
-              <p className="mt-2">${product.price}</p>
-              <button
-                onClick={() => onClickCart(product)}
-                disabled={product.qty === 0}
-                className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Add to Cart
-              </button>
+              {selectedProduct && selectedProduct.id === product.id && (
+                <>
+                  <p className="mt-2">{product.description}</p>
+                  <p className="mt-2">Quantity: {product.qty}</p>
+                  <p className="mt-2">${product.price}</p>
+                  <button
+                    onClick={() => onClickCart(product)}
+                    disabled={product.qty === 0}
+                    className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Add to Cart
+                  </button>
+                  <button
+                    onClick={() => toggleProductDetails(product)}
+                    className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+                  >
+                    Go Back
+                  </button>
+                </>
+              )}
             </div>
           ))}
         </div>
